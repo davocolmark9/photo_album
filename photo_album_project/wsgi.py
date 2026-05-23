dbd1a832879e8c9eca2ac45682a2fbd490d57038
davocolmark9 <davocolmark9@gmail.com>
2026-05-23 19:58:23 +0800

    Fix Render 400 error

--- photo_album_project/wsgi.py
+++ photo_album_project/wsgi.py
@@ -4,3 +4,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo_album_project.settings')
 
 application = get_wsgi_application()
+
+# Run migrations on startup (only in production)
+if os.environ.get('RENDER') == 'true':
+    from django.core.management import call_command
+    call_command('migrate', '--noinput')
